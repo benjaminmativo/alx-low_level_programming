d8688aed7bc0304204d06d8f74137b73d1342470
benjaminmativo <benjaminmativomusau@gmail.com>
2022-09-12 03:45:12 -0700

    4 task update

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,25 +2,21 @@
 /**
  * main - Entry point
  *
- *  Return: Always 0 (Success/correct)
+ * Description: using the main function
+ * this program prints "programming is positive, zero, or negative
+ * Return: 0
  */
 int main(void)
-
 {
-char alphabet;
-
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-
-	if (alphabet -- 'q')
-		continue;
-	
-	else if (alphabet == 'e')
-		continue;
-
-	putchar (alphabet);
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+if (ch != 'e' && ch != 'q')
+{
+	putchar(ch);
 }
-
-putchar ('\n');
-
+}
+putchar('\n');
 return (0);
 }
+
