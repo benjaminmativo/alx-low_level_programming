9e96db92d53a1ce9ce0972aef8652b9973dc0305
benjaminmativo <benjaminmativomusau@gmail.com>
2022-09-21 10:53:42 -0700

    Task 0 done

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,11 +2,11 @@
 
 /**
  *_strcat - concatenates the string pointed to by @src to
- *the end of the string pointed to by @dest
- *@dest: String that will be appended
+ * the end of the string pointed to by @dest
+ *@dest: string that will be appended
  *@src: String to be concatenated upon
  *
- * Return: returns pointer to @dest
+ * Returns poiner to @dest
  */
 
 char *_strcat(char *dest, char *src)
@@ -14,7 +14,7 @@ char *_strcat(char *dest, char *src)
 
 	int index = 0, dest_len = 0;
 
-	while (dest[index+])
+	while (dest[index++])
 		dest_len++;
 
 	for (index = 0; src[index]; index++)
