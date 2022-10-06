6e1a19f0b0b6d33963b97a9563d35a9ccdb6b405
Benjmain Mativo <110989615+benjaminmativo@users.noreply.github.com>
2022-10-06 20:37:53 +0300

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -26,11 +25,11 @@ int find_len(char *str)
 
 /**
  * create_xarray - Creates an array of chars and initializes it with
- *                 the character 'x'. Adds a terminating null byte.
+ *                           the character 'x'. Adds a terminating null byte.
  * @size: The size of the array to be initialized.
  *
  * Description: If there is insufficient space, the
- *              function exits with a status of 98.
+ *                      function exits with a status of 98.
  * Return: A pointer to the array.
  */
 char *create_xarray(int size)
@@ -53,17 +52,17 @@ char *create_xarray(int size)
 
 /**
  * iterate_zeroes - Iterates through a string of numbers containing
- *                  leading zeroes until it hits a non-zero number.
+ *                           leading zeroes until it hits a non-zero number.
  * @str: The string of numbers to be iterate through.
  *
  * Return: A pointer to the next non-zero element.
  */
 char *iterate_zeroes(char *str)
 {
-	while (*str && *str == '0')
-		str++;
+		while (*str && *str == '0')
+					str++;
 
-	return (str);
+			return (str);
 }
 
 /**
@@ -71,7 +70,7 @@ char *iterate_zeroes(char *str)
  * @c: The character to be converted.
  *
  * Description: If c is a non-digit, the function
- *              exits with a status of 98.
+ *                      exits with a status of 98.
  * Return: The converted int.
  */
 int get_digit(char c)
@@ -95,7 +94,7 @@ int get_digit(char c)
  * @zeroes: The necessary number of leading zeroes.
  *
  * Description: If mult contains a non-digit, the function
- *              exits with a status value of 98.
+ *                      exits with a status value of 98.
  */
 void get_prod(char *prod, char *mult, int digit, int zeroes)
 {
@@ -184,7 +183,8 @@ void add_nums(char *final_prod, char *next_prod, int next_len)
  * @argc: An array of pointers to the arguments.
  *
  * Description: If the number of arguments is incorrect or one number
- *              contains non-digits, the function exits with a status of 98.
+ *                      contains non-digits, the function 
+ *                      exits with a status of 98.
  * Return: Always 0.
  */
 int main(int argc, char *argv[])
